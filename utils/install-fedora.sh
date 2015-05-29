f5229e4468ca771ea48e05952250c864471f1280
Tim Bovelander <tim.bovelander@kpn.com>
2015-05-29 12:30:23 +0200

    Utils instead of scripts folder

--- utils/install-fedora.sh
+++ utils/install-fedora.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 
 # Clone dotfiles
+sudo dnf install git
 git clone https://github.com/timbovelander/dotfiles.git "$HOME/.dotfiles"
 
 # Install fonts
 sudo dnf install -y google-droid-sans-mono-fonts
 
-# Install install dependencies
-sudo dnf install -y git rsync cmake python-devel
-
 # Install ZSH
 sudo dnf install -y zsh autojump-zsh
-sh "$HOME/.dotfiles/scripts/install-zsh.sh"
+sh "$HOME/.dotfiles/utils/install-zsh.sh"
 
 # Install NodeJS
 sudo dnf install -y nodejs npm
-sh "$HOME/.dotfiles/scripts/install-node.sh"
+sh "$HOME/.dotfiles/utils/install-node.sh"
 
 # Install ViM
-sudo dnf install -y vim-X11 vim-enhanced
-sh "$HOME/.dotfiles/scripts/install-vim.sh"
+sudo dnf install -y vim-X11 vim-enhanced cmake python-devel
+sh "$HOME/.dotfiles/utils/install-vim.sh"
 
 # Install RVM
-sh "$HOME/.dotfiles/scripts/install-rvm.sh"
+sh "$HOME/.dotfiles/utils/install-rvm.sh"
 
 # Copy dotfiles
+sudo dnf install rsync
 rsync -r --exclude-from "$HOME/.dotfiles/utils/exclude-files" $HOME/.dotfiles/ $HOME/
