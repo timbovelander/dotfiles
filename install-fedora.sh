043c1db71511a29c09e9c7503844f645aabee054
Tim Bovelander <tim.bovelander@kpn.com>
2015-05-29 10:28:57 +0200

    Add building of vundles

--- install-fedora.sh
+++ install-fedora.sh
@@ -31,3 +31,12 @@ rvm @global do gem install scss-lint
 # Install Vundles
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u $HOME/.dotfiles/.vimrc_vundle +PluginInstall +qall
+
+# Build Vundle YouCompleteMe
+mkdir /tmp/ycm_build && cd /tmp/ycm_build
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_support_libs
+
+# Build Vundle tern_for_vim
+cd $HOME/.vim/bundle/tern_for_vim
+npm install
