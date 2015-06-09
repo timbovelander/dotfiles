96c7a5b2f44c0d1cab61e07c2023200a9845bf9a
Tim Bovelander <tim.bovelander@kpn.com>
2015-06-09 15:43:32 +0200

    Remove temporary files after installing YCM

--- utils/install-vim.sh
+++ utils/install-vim.sh
@@ -11,6 +11,7 @@ build() {
   mkdir /tmp/ycm_build && cd /tmp/ycm_build
   cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
   make ycm_support_libs
+  rm -Rf /tmp/ycm_build
 
   # Build Vundle tern_for_vim
   cd $HOME/.vim/bundle/tern_for_vim
