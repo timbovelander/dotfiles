c9086393d27c0b461e13b878d40b404b51228ad7
Tim Bovelander <tim@timenwietske.nl>
2015-07-23 15:07:32 +0200

    Add show, ff and fd aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,9 +1,25 @@
-# alias cat to pygmentize if available
-if command -v pygmentize >/dev/null 2>&1; then
-  alias cat="pygmentize -g"
-fi
+# find directories
+function fd {
+  find -type d -name "*$1*"
+}
+
+# find files
+function ff {
+  find -type f -name "*$1*"
+}
 
 # alias less to vimless if available
 if [ -f /usr/share/vim/vim74/macros/less.sh ]; then
   alias less="/usr/share/vim/vim74/macros/less.sh"
 fi
+
+# show file with pygmentize or cat
+function show {
+  echo ''
+  if command -v pygmentize >/dev/null 2>&1; then
+    pygmentize -g "$1"
+  else
+    cat "$1"
+  fi
+  echo ''
+}
