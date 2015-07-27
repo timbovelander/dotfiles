986564c77d12fb2d7016d9e1240f4c07db078db6
Tim Bovelander <tim@timenwietske.nl>
2015-07-27 21:14:26 +0200

    Another try to fix setup

--- utils/install-rvm.sh
+++ utils/install-rvm.sh
@@ -3,11 +3,11 @@
 if ! command -v rvm &>/dev/null; then
   # Add key
   gpg2 --keyserver hkp://keys.gnupg.net --recv-keys \
-  409B6B1796C275462A1703113804BB82D39DC0E3 >/dev/null
+  409B6B1796C275462A1703113804BB82D39DC0E3
 
   # Install RVM
   echo "Installing RVM..."
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
+  curl -sSL https://get.rvm.io | bash -s stable
 
   # Source RVM
   source "$HOME/.rvm/scripts/rvm"
