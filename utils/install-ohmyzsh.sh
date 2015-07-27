59372f362127eb67b5b0306545f2dd67644d1e83
Tim Bovelander <tim@timenwietske.nl>
2015-07-27 15:51:39 +0200

    Fix curl oh-my-zsh command

--- utils/install-ohmyzsh.sh
+++ utils/install-ohmyzsh.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL
-https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
->/dev/null
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null
