3194525bc8ee64cb22066bae1cb593d1e2c5825d
Tim Bovelander <tim@timenwietske.nl>
2015-07-27 15:49:34 +0200

    Fix gpg2 key entry

--- utils/install-rvm.sh
+++ utils/install-rvm.sh
@@ -2,7 +2,7 @@
 
 if ! command -v rvm &>/dev/null; then
   # Add key
-  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys
+  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys \
   409B6B1796C275462A1703113804BB82D39DC0E3 >/dev/null
 
   # Install RVM
