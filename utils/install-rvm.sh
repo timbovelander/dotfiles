2e06758422cf964f14eef79b5e08bbf0d4542cb4
Tim Bovelander <tim@timenwietske.nl>
2015-07-27 21:38:55 +0200

    Change way to install global gems

--- utils/install-rvm.sh
+++ utils/install-rvm.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 
 if ! command -v rvm &>/dev/null; then
+  globalgems="scss_lint"
+
   # Add key
   gpg2 --keyserver hkp://keys.gnupg.net --recv-keys \
   409B6B1796C275462A1703113804BB82D39DC0E3
 
   # Install RVM
   echo "Installing RVM..."
-  curl -sSL https://get.rvm.io | bash -s stable
-
-  # Source RVM
-  source "$HOME/.rvm/scripts/rvm"
-fi
-
-# Install global gems
-if command -v rvm &>/dev/null; then
-  while read gem; do
-    if ! rvm @global do gem list -i $gem >/dev/null; then
-
-      echo "Installing global gem $gem..."
-      rvm @global do gem install $gem >/dev/null
-
-    fi
-  done <"$HOME/.dotfiles/utils/ruby-gems"
+  curl -sSL https://get.rvm.io | bash -s stable --ruby \
+  --with-gems="$globalgems"
 fi
