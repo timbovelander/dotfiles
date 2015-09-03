90eab1f9f7c550c9760d1ac52cdfb4a3dcbfed9a
Tim Bovelander <tbovelander@bol.com>
2015-09-03 16:13:07 +0200

    Add update function

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -8,4 +8,27 @@ ips() {
   fi
 }
 
+update() {
+  pwd=$(pwd)
 
+  # update distribution
+  sudo apt-get update
+  sudo apt-get dist-upgrade
+
+  # update vim
+  if command -v gvim &>/dev/null; then
+    gvim +PluginUpdate +qall
+  fi
+
+  # update node modules
+  if command -v npm &>/dev/null; then
+    sudo npm -g update
+    if [ -d "$HOME/.vim/bundle/tern_for_vim" ]; then
+      cd "$HOME/.vim/bundle/tern_for_vim"
+      npm update
+    fi
+  fi
+
+  # change back to working directory
+  cd $pwd
+}
