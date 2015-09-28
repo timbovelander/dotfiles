0fb343ec99d77684c087573f6cc7845a33790be7
Tim Bovelander <tbovelander@bol.com>
2015-09-28 15:52:04 +0200

    Improve vi-mode plugin and set up a way to display vi-mode in GNU screen

--- .oh-my-zsh/custom/plugins/vi-mode/vi-mode.plugin.zsh
+++ .oh-my-zsh/custom/plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,9 +1,13 @@
 # use vi-mode in zsh
 bindkey -v
 
+# kill lag when changing mode
+export KEYTIMEOUT=1
+
 # redraw prompt when vi-mode changes
 function zle-line-init zle-keymap-select {
   zle reset-prompt
+  vi_mode_screen_info
 }
 zle -N zle-line-init
 zle -N zle-keymap-select
@@ -23,4 +27,13 @@ function vi_mode_prompt_info() {
   VI_MODE="${KEYMAP/vicmd/$ZSH_THEME_VI_MODE_NORMAL}"
   VI_MODE="${VI_MODE/(main|viins)/$ZSH_THEME_VI_MODE_INSERT}"
   echo $VI_MODE
+}
+
+# print active vi-mode in file for screen to use
+function vi_mode_screen_info() {
+  if [ "$KEYMAP" = "vicmd" ]; then
+    echo "NORMAL" > "/tmp/zsh-vimode-screen"
+  else
+    rm -Rf "/tmp/zsh-vimode-screen"
+  fi
 }
