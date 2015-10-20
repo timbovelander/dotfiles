c89554b883b880be314b7a1eeae24f18d114cbfb
Tim Bovelander <tbovelander@bol.com>
2015-10-20 21:21:15 +0200

    Change some installation settings because of different vim plugins

--- utils/install-vim.sh
+++ utils/install-vim.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 init() {
   # Install vundles
   echo "Cloning Vundle to ~/.vim/bundle/Vundle.vim..."
   git clone -q https://github.com/gmarik/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
   echo "Installing and configuring VIM Vundles..."
-  gvim -u "$HOME/.dotfiles/.vimrc" +PluginInstall -c "!sh $HOME/.dotfiles/utils/install-vim.sh build" +qall
+  vim -u "$HOME/.dotfiles/.vimrc" +PluginInstall -c "!sh $HOME/.dotfiles/utils/install-vim.sh build" +qall
 }
 
 build() {
-  # Build Vundle YouCompleteMe
-  mkdir "/tmp/ycm_build" && cd "/tmp/ycm_build"
-  cmake -G "Unix Makefiles" . "$HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp"
-  make ycm_support_libs
-
   # Build vimproc
   cd "$HOME/.vim/bundle/vimproc.vim"
   make
