076569180bd7c312eaa189682a94f4ed21e97b4e
Tim Bovelander <tim@timenwietske.nl>
2016-03-25 11:24:01 +0100

    Improve fasd functions and add tab completion

--- home/.local/share/fasd/fasd.fish
+++ home/.local/share/fasd/fasd.fish
@@ -1,35 +1,75 @@
-function fasd_init -e fish_preexec
-  fasd --proc (fasd --sanitize $argv | sed "s/\s/\n/g") > /dev/null 2>&1
+function __fasd_init -e fish_preexec
+  command fasd --proc (command fasd --sanitize $argv) > "/dev/null" 2>&1 &
 end
 
-function ea
-  set -l any (fasd -a -e "printf %s" "$argv")
-  set_color blue
-  echo "$any"
-  set_color normal
-  eval e "$any"
+function __fasd_print_completions
+  set cmd (commandline -op)
+  if test (count $cmd) -ge 2
+    command fasd -l $argv $cmd[2..-1]
+  else
+    command fasd -l $argv
+  end
 end
 
-function ed
-  set -l dir (fasd -d -e "printf %s" "$argv")
-  set_color blue
-  echo "$dir"
-  set_color normal
-  eval e "$dir"
+function fasd_cd
+  if test (count $argv) -le 1
+    command fasd $argv
+  else
+    set -l ret (command fasd -e 'printf %s' $argv)
+    test -z "$ret"; and return
+    test -d "$ret"; and cd "$ret"; or printf "%s\n" $ret
+  end
 end
 
-function ef
-  set -l file (fasd -f -e "printf %s" "$argv")
-  set_color blue
-  echo "$file"
-  set_color normal
-  eval e "$file"
+function a
+  command fasd -a $argv
+end
+
+function d
+  command fasd -d $argv
+end
+
+function f
+  command fasd -f $argv
+end
+
+function s
+  command fasd -si $argv
+end
+
+function sd
+  command fasd -si $argv
+end
+
+function sf
+  command fasd -si $argv
 end
 
 function j
-  set -l dir (fasd -d -e "printf %s" "$argv")
-  set_color blue
-  echo "$dir"
-  set_color normal
-  eval cd "$dir"
+  fasd_cd -d $argv
 end
+
+function ea
+  command fasd $argv -e "emacsclient -c -a emacs"
+end
+
+function ed
+  command fasd -d $argv -e "emacsclient -c -a emacs"
+end
+
+function ef
+  command fasd -f $argv -e "emacsclient -c -a emacs"
+end
+
+complete -c fasd -a "(__fasd_print_completions)" -f
+complete -c fasd_cd -a "(__fasd_print_completions -d)" -f -A
+complete -c a -a "(__fasd_print_completions -a)" -f -A
+complete -c d -a "(__fasd_print_completions -d)" -f -A
+complete -c f -a "(__fasd_print_completions -f)" -f -A
+complete -c s -a "(__fasd_print_completions)" -f -A
+complete -c sd -a "(__fasd_print_completions -d)" -f -A
+complete -c sf -a "(__fasd_print_completions -f)" -f -A
+complete -c j -a "(__fasd_print_completions -d)" -f -A
+complete -c ea -a "(__fasd_print_completions)" -f -A
+complete -c ed -a "(__fasd_print_completions -d)" -f -A
+complete -c ef -a "(__fasd_print_completions -f)" -f -A
