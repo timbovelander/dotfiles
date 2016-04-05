e3b340944e11a96405aa43f713c426ecb1cb5863
Tim Bovelander <tim@timenwietske.nl>
2016-04-05 09:45:34 +0200

    Fix update function and scroll indefinite in terminator

--- home/.config/fish/functions/update.fish
+++ home/.config/fish/functions/update.fish
@@ -4,17 +4,14 @@ function update
   sudo apt-get update
   sudo apt-get dist-upgrade
 
-  vim +PluginUpdate +qall
-
-  cd "$HOME/.vim/bundle/vimproc.vim"
-  make
-
-  cd "$HOME/.vim/bundle/tern_for_vim"
-  npm update
-
   sudo npm -g update
 
-  apm update
+  cd "$HOME/.dotfiles"
+  git pull
+  bash "$HOME/.dotfiles/scripts/symlinks.sh"
+
+  cd "$HOME/.emacs.d"
+  git pull
 
   cd $current_directory
 end
