a24163ceeea5268b5ccd404f12246ab1c594f621
Tim Bovelander <tbovelander@bol.com>
2016-08-05 14:52:41 +0200

    Fix fasd install script

--- scripts/fasd.sh
+++ scripts/fasd.sh
@@ -2,6 +2,7 @@
 
 if ! command -v fasd &>/dev/null; then
   wget -q -O /tmp/fasd.tar.gz https://github.com/clvv/fasd/tarball/1.0.1
+  cd "/tmp"
   tar -xzf /tmp/fasd.tar.gz --transform="s|^.*/|fasd/|"
   cd "/tmp/fasd"
   sudo make install
