d8a58f1f9d94007472770db97c7622ba98787c3c
Tim Bovelander <tbovelander@bol.com>
2016-08-09 13:18:10 +0200

    Install fisherman plugins during setup

--- scripts/fisherman.sh
+++ scripts/fisherman.sh
@@ -2,4 +2,7 @@
 
 set -u
 
-curl -Lo "$HOME/.config/fish/functions/fisher.fish" --create-dirs "git.io/fisher"
+if ! command -v fish &>/dev/null; then
+  curl -Lo "$HOME/.config/fish/functions/fisher.fish" --create-dirs "git.io/fisher"
+  fish -c "fisher"
+fi
