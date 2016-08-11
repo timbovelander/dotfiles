2aa24156211e5d0cc4daefbd35854743535f9a79
Tim Bovelander <tim@timenwietske.nl>
2016-08-11 13:56:04 +0200

    Fix fisherman install script

--- scripts/fisherman.sh
+++ scripts/fisherman.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-if ! command -v fish &>/dev/null; then
+if command -v fish &>/dev/null; then
   curl -Lo "$HOME/.config/fish/functions/fisher.fish" --create-dirs "git.io/fisher"
   fish -c "fisher"
 fi
