793c0db5b3439b6fa52893adf25e77d5d90626ac
Tim Bovelander <tim@timenwietske.nl>
2016-09-08 23:09:09 +0200

    Add brew to update script and use rebase for git pull

--- home/.config/fish/functions/update.fish
+++ home/.config/fish/functions/update.fish
@@ -8,6 +8,10 @@ function update
     sudo zypper update
   end
 
+  if command -s brew >/dev/null ^&1
+    brew update
+  end
+
   if command -s npm >/dev/null ^&1
     npm -g update
   end
@@ -21,15 +25,22 @@ function update
   end
 
   if command -s git >/dev/null ^&1
-    test -d "$HOME/.dotfiles"; and git -C "$HOME/.dotfiles" pull
-    test -d "$HOME/.emacs.d"; and git -C "$HOME/.emacs.d" pull
-    test -d "$HOME/.rbenv"; and git -C "$HOME/.rbenv" pull
-    test -d "$HOME/.rbenv/plugins/ruby-build"
-    and git -C "$HOME/.rbenv/plugins/ruby-build" pull
-    test -d "$HOME/.rbenv/plugins/rbenv-gemset"
-    and git -C "$HOME/.rbenv/plugins/rbenv-gemset" pull
+    test -d "$HOME/.dotfiles"
+    and git -C "$HOME/.dotfiles" pull --rebase=preserve --prune
 
-    test -f "$HOME/.dotfiles/scripts/symlinks.sh"
-    and bash "$HOME/.dotfiles/scripts/symlinks.sh"
+    test -d "$HOME/.emacs.d"
+    and git -C "$HOME/.emacs.d" pull --rebase=preserve --prune
+
+    test -d "$HOME/.rbenv"
+    and git -C "$HOME/.rbenv" pull --rebase=preserve --prune
+
+    test -d "$HOME/.rbenv/plugins/ruby-build"
+    and git -C "$HOME/.rbenv/plugins/ruby-build" pull --rebase=preserve --prune
+
+    test -d "$HOME/.rbenv/plugins/rbenv-gemset"
+    and git -C "$HOME/.rbenv/plugins/rbenv-gemset" pull --rebase=preserve --prune
   end
+
+  test -f "$HOME/.dotfiles/scripts/symlinks.sh"
+  and bash "$HOME/.dotfiles/scripts/symlinks.sh"
 end
