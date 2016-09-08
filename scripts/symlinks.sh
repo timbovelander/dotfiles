3f0e7de473f2651f5a77eee468e716c7b8b0c642
Tim Bovelander <tim@timenwietske.nl>
2016-09-08 23:07:04 +0200

    Fix symlinks script for OSX

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # create all directories
-find "$HOME/.dotfiles/home" -type d | sed -r 's/(.*)\/\.dotfiles\/home/\1/' | xargs mkdir -p
+find "$HOME/.dotfiles/home" -type d | sed 's/\.dotfiles\/home\///' | xargs mkdir -p
 
 # create symlinks for all files, backup old files
 while read src; do
-  target=$(echo $src | sed -r 's/(.*)\/\.dotfiles\/home/\1/')
-  ln -sfb "$src" "$target"
+  target=$(echo $src | sed 's/\.dotfiles\/home\///')
+  mv "$target" "$target.bak"
+  ln -sf "$src" "$target"
 done < <(exec find "$HOME/.dotfiles/home" -type f)
