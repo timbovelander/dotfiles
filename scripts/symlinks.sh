cf92962b676039ad8480e4f712b1e8ff5a5bc319
Tim Bovelander <tbovelander@bol.com>
2016-09-16 15:33:30 +0200

    Do not backup symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -6,6 +6,8 @@ find "$HOME/.dotfiles/home" -type d | sed 's/\.dotfiles\/home\///' | xargs mkdir
 # create symlinks for all files, backup old files
 while read src; do
   target=$(echo $src | sed 's/\.dotfiles\/home\///')
-  mv "$target" "$target.bak"
+  if [[ ! -L "$target" && -f "$target" ]]; then
+    mv "$target" "$target.bak"
+  fi
   ln -sf "$src" "$target"
 done < <(exec find "$HOME/.dotfiles/home" -type f)
