bf269235be4114d7a44971757ddfda6e2c3c48af
Tim Bovelander <tim@startmail.com>
2018-11-01 11:54:19 +0100

    Change ips function to show the local ip address correctly

--- home/.config/fish/functions/ips.fish
+++ home/.config/fish/functions/ips.fish
@@ -1,13 +1,11 @@
 function ips
   if set ip_external (dig +short +time=1 myip.opendns.com @resolver1.opendns.com ^/dev/null)
-    printf "ext\t$ip_external\n"
+    printf "$ip_external\n"
   end
 
   if command -s ip >/dev/null ^&1
-    ip -o -4 address show up |\
-    grep -Po "(?<=^\d:\s)([^\s]+).*inet\s*([\d\.]*)" |\
-    sed -e "s/\s*inet\s*/\t/"
+    printf "%s\n" (ip route get 1 | head -1 | cut -d' ' -f7)
   else if command -s ipconfig >/dev/null ^&1
-    printf "en0\t%s\n" (ipconfig getifaddr en0 )
+    printf "%s\n" (ipconfig getifaddr en0 )
   end
 end
